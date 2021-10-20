5fcf7f6ed558b3ca785c2fb49515d5093e9c0c6c
Ankit Srivastava <ankit.kumar@mediakind.com>
2021-10-20 21:45:26 +0530

    Adding AdjancyMatrix Graph

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 using namespace std;
 
 class Node {
@@ -25,6 +26,20 @@ void preorder (Node * root) {
     preorder(root->right);
 }
 
+void preorderIterative(Node *root) {
+      stack<Node *> s;
+      while(true) {
+          while (root != nullptr) {
+              cout<<root->data<<" ";
+              s.push(root);
+              root = root->left;
+          }
+          if(s.empty()) break;
+          root = s.top(); s.pop();
+          root = root->right;
+      }
+}
+
 void inorder (Node * root) {
     if (root == nullptr)
         return;
@@ -34,12 +49,45 @@ void inorder (Node * root) {
     inorder(root->right);
 }
 
+void inorderIterative(Node *root) {
+    stack<Node *> s;
+    while(true) {
+        while(root != nullptr) {
+            s.push(root);
+            root = root->left;
+        }
+        if(s.empty()) break;
+        root = s.top(); s. pop();
+        cout<<root->data<<" ";
+        root = root->right;
+    }
+}
+
 void postorder (Node * root) {
     if (root == nullptr)
         return;
     postorder(root->left);
     postorder(root->right);
     cout<<root->data<<" ";
+}
+
+void postorderIterative(Node *root) {
+    stack<Node *>s;
+    while(true) {
+        while(root != nullptr) {
+            s.push(root);
+            s.push(root);
+            root = root->left;
+        }
+        if(s.empty()) break;
+        root = s.top(); s.pop();
+        if(!s.empty() && root == s.top()) 
+         root = root->right;
+        else {
+         cout<<root->data<<" ";
+         root = nullptr;
+        }
+    }
 }
 
 void levelOrder(Node *root) {
@@ -91,7 +139,49 @@ int height(Node *root) {
         return right+1;
     }
 }
- 
+
+int Diameter(Node *root) {
+    if (root == nullptr) 
+     return 0;
+
+    int f = height(root->left) + height(root->right) +1;
+    int lD = Diameter(root->left);
+    int rD = Diameter(root->right);
+    return max(f, max(lD, rD)); 
+}
+
+int maxSum(Node *root) {
+    if (root == nullptr) {
+       return 0;
+    }
+    int max_sum = 0, currentSum=0;
+    std::queue<Node *> queue;
+    queue.push(root);
+    queue.push(nullptr);
+    while(queue.empty() ==  false) {
+        Node *temp = queue.front(); queue.pop();
+        if (temp == nullptr) {
+            max_sum = max(max_sum, currentSum);
+            currentSum = 0;
+            if(!queue.empty()) {
+                queue.push(nullptr);
+            }
+        } else {
+            if (temp != nullptr) {
+                currentSum += temp->data;
+            }
+
+            if (temp->left) {
+                queue.push(temp->left);
+            }
+            if(temp->right) {
+                queue.push(temp->right);
+            }
+        }
+    }
+    return max_sum;
+}
+
 void DeleteTree (Node * root) {
     if (root == nullptr)
         return;
@@ -101,25 +191,66 @@ void DeleteTree (Node * root) {
     root = nullptr;
 }
 
+Node * LCA(Node *root, Node *s1, Node *s2) {
+    if (root == nullptr)
+        return nullptr;
+
+    if ( root == s1 || root == s2) {
+        return root;
+    }
+
+    Node *left = LCA(root->left, s1, s2);
+    Node *right = LCA(root->right, s1, s2);
+    if(left && right) {
+        return root;
+    }
+    return left ? left :right;
+}
+
+bool PrintAllAncestors(Node *root , Node *s) {
+    if ( root == nullptr) {
+        return false;
+    }
+    if (root->left == s || root ->right ==  s || PrintAllAncestors(root->left,s) || PrintAllAncestors(root->right,s)) {
+        cout<<root->data<<" ";
+        return true;
+    }
+    return false;
+}
 
 int main() {
-    Node *root = new Node(10);
-    root->left = new Node(20);
-    root->right = new Node(30);
-
-    root->left->left = new Node(40);
-    root->left->right = new Node(50);
-    root->left->right->left = new Node(80);
-
-    root->right->left = new Node(60);
-    root->right->right = new Node(70);
+    Node *s1, *s2;
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->left->left->left = s1 = new Node(8);
+    root->left->left->right = new Node(9);
+    root->left->right->left = s2 =new Node(10);
+    root->left->right->right = new Node(11);
+    root->right->left->left = new Node(12);
+    root->right->left->right = new Node(13);
+    root->right->right->left = new Node(14);
+    root->right->right->right = s2 = new Node(15);
     cout<<"Preorder "; preorder(root); cout<<endl;
     cout<<"Inorder "; inorder(root); cout<<endl;
     cout<<"PostOrder "; postorder(root); cout<<endl;
+    cout<<"Preorder Iterative "; preorderIterative(root); cout<<endl;
+    cout<<"Inorder Iterative "; inorderIterative(root); cout<<endl;
+    cout<<"Postorder Iterative "; postorderIterative(root); cout<<endl;
     cout<<"LevelOrder "; levelOrder(root); cout<<endl;
     cout<<"Size "<<TreeSize(root); cout<<endl;
     cout<<"Height "<<height(root); cout<<endl;
+    cout<<"Diameter "<<Diameter(root)<<endl;
+    cout<<"Max Sum "<<maxSum(root)<<endl;
+    // print constructed binary tree
+    cout<<"Common Ancestor "<<LCA(root, s1, s2)->data<<endl;
+    cout<<"PrintAllAncestors "<<PrintAllAncestors(root, s2)<<endl;
     cout<<"Deleting the tree "; DeleteTree(root); cout<<endl;
+
     return 0;
 }
 
---
+++ Graph/AdjancyMatrix.cpp
@@ -0,0 +1,93 @@
+/*
+     A-------C----------|
+     |       |          |
+     |       |          |
+     B-------D----E     |
+             |    |     |
+             |    |     |
+             F----G-----|
+
+Node are A B C D E F G
+*/
+
+#include <queue>
+#include <iostream>
+#include <memory>
+#define V 7
+#define VISITED 1
+#define NOT_VISITED 0
+void printGraph(int graph[][V]) {
+    std::cout<<"    ";
+   for (int i=0; i< V; i++) 
+        std::cout<<" ("<<i<<") ";
+    std::cout<<std::endl;    
+
+   for (int i=0; i<V; i++) {
+    std::cout<<"("<<i<<") : ";
+    for (int edges : graph[i]) {
+        std::cout<<""<<edges<<"    ";
+    }
+    std::cout<<std::endl;   
+   }
+}
+
+void addEdge(int graph[][V], int V1, int V2, int weight=1){
+    // Adding Edge O(1)
+    graph[V1][V2] = weight;
+    graph[V2][V1] = weight;
+}
+
+void removeEdge(int graph[][V], int V1, int V2){
+    // Removig edge O(1)
+    graph[V1][V2] = 0;
+    graph[V2][V1] = 0;
+}
+
+bool isConnected(int graph[V][V], int V1, int V2) {
+    // Query O(1)
+    return graph[V1][V2];
+}
+
+void bfs(int graph[][V], int startNode) {
+    std::cout<<"\nBFS : ";
+    std::queue<int> q;
+    int isVisited[V] {NOT_VISITED};
+    q.push(startNode);
+    isVisited[startNode] = VISITED;
+    while (!q.empty()) {
+        const int v = q.front(); q.pop();
+        std::cout<<v<<" ";
+        for (int i=0; i<V; i++) {
+            if (graph[v][i] == 1 && isVisited[i] == NOT_VISITED) {
+                q.push(i);
+                isVisited[i] = VISITED;
+            }  
+        }
+    }
+    std::cout<<"\n";
+} 
+
+int main() {
+   //Adjency matrix Undirected Graph
+   int graph[V][V] {0};
+   addEdge(graph, 0, 1);
+   addEdge(graph, 0, 2);
+   addEdge(graph, 1, 3);
+   addEdge(graph, 2, 3);
+   addEdge(graph, 2, 6);
+   addEdge(graph, 3, 4);
+   addEdge(graph, 3, 5);
+   addEdge(graph, 4, 6);
+   addEdge(graph, 5, 6);
+   std::cout<<"Is 2 and 6 Connected "<<isConnected(graph, 2, 6)<<"\n";
+   
+   printGraph(graph);
+   bfs(graph, 0);
+   
+   removeEdge(graph, 2, 6);
+   std::cout<<"Is 2 and 6 Connected "<<isConnected(graph, 2, 6)<<"\n";
+   
+   printGraph(graph);
+   bfs(graph, 0);
+   return 0;
+}
