6431ef995cf8e3eaaf3e840b743dd315cf61b229
Ankit Srivastava <ankit.kumar@mediakind.com>
2021-12-23 15:24:47 +0530

    Equal Sum Partition: Partition problem is to determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is the same.

---
+++ Dynamic Programming/Knapsack/EqualSumPartition.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <numeric>   
+#include <vector>
+using namespace std;
+
+bool isSubsetSum(vector<int>arr, int sum) {
+    int n = arr.size();
+    vector<vector<bool>> t(n+1, vector<bool>(sum+1));
+    // initialise vector
+    for (int i=0; i<=sum; i++) {
+        t[0][i] = false; 
+    }
+    for (int i=0; i<=n; i++) {
+        t[i][0] = true; 
+    }
+
+    // create DP table
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=sum; j++) {
+            if(arr[i-1] <= j) {
+                t[i][j] = t[i-1][j] || t[i-1][j-arr[i-1]];
+            } else {
+                t[i][j] = t[i-1][j];
+            }
+        }
+    }
+    /*
+    for (vector<bool> oneD : t) {
+        cout<<endl;
+        for (bool val : oneD) {
+            cout<< ((val==true)? 'T':'F')<<" ";
+        }
+    }
+    cout<<endl;
+    */
+    return t[n][sum];
+}
+
+bool isEqualSumPartition(vector<int>arr) {
+    int sum = accumulate(arr.begin(),arr.end(), 0);
+    // if total is even then only we are divide into two subpart.
+    if(sum%2 != 0) {
+        return false;
+    } else {
+        return isSubsetSum(arr,sum/2);
+    }
+}
+
+int main() {
+    vector<int>arr {1,5,11,5};
+    cout<<isEqualSumPartition(arr)<<endl;
+}
