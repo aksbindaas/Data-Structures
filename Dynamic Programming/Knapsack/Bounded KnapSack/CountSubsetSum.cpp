52ef648f66cffda7ef8f8629935d0586d44caee1
Ankit Srivastava <ankit.kumar@mediakind.com>
2022-01-07 19:43:31 +0530

    Adding recursive coding

--- Dynamic Programming/Knapsack/Bounded KnapSack/CountSubsetSum.cpp
+++ Dynamic Programming/Knapsack/Bounded KnapSack/CountSubsetSum.cpp
@@ -3,6 +3,16 @@
 #include<vector>
 using namespace std;
 
+int countSubsetSumProblem(vector<int> arr, int sum, int n) {
+    if(sum == 0) return 1;
+    if(n == 0) return 0;
+    if(arr[n-1] > sum) {
+        return countSubsetSumProblem(arr, sum , n-1);
+    } else {
+        return countSubsetSumProblem(arr, sum , n-1)+countSubsetSumProblem(arr, sum-arr[n-1], n-1);
+    }
+}
+
 int countSubsetSumProblem(vector<int> arr, int sum) {
     int n = arr.size();
     vector<vector<int>>t(n+1, vector<int>(sum+1,0));
@@ -35,4 +45,5 @@ int main() {
     vector<int> arr {2,3,5,6,8,10};
     int sum = 10;
     cout<<countSubsetSumProblem(arr, sum)<<endl;
+    cout<<countSubsetSumProblem(arr, sum, arr.size())<<endl;
 }
