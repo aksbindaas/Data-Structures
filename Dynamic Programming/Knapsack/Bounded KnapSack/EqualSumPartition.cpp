53c97a6fb9e58812da14d035e718283a4699bf56
Ankit Srivastava <ankit.kumar@mediakind.com>
2022-01-07 19:40:16 +0530

    Adding Recursive code

--- Dynamic Programming/Knapsack/Bounded KnapSack/EqualSumPartition.cpp
+++ Dynamic Programming/Knapsack/Bounded KnapSack/EqualSumPartition.cpp
@@ -3,6 +3,16 @@
 #include <vector>
 using namespace std;
 
+bool isSubsetSum(vector<int>arr, int sum, int n) {
+    if (sum == 0) return true;
+    if (n == 0)   return false;
+    if(arr[n-1] > sum) {
+        return isSubsetSum(arr, sum , n-1);
+    } else {
+        return isSubsetSum(arr, sum , n-1) || isSubsetSum(arr, sum- arr[n-1], n-1);
+    }
+}
+
 bool isSubsetSum(vector<int>arr, int sum) {
     int n = arr.size();
     vector<vector<bool>> t(n+1, vector<bool>(sum+1));
@@ -36,17 +46,25 @@ bool isSubsetSum(vector<int>arr, int sum) {
     return t[n][sum];
 }
 
-bool isEqualSumPartition(vector<int>arr) {
+bool isEqualSumPartition(vector<int>arr, bool isRecursive = false) {
     int sum = accumulate(arr.begin(),arr.end(), 0);
     // if total is even then only we are divide into two subpart.
     if(sum%2 != 0) {
         return false;
     } else {
-        return isSubsetSum(arr,sum/2);
+        if(isRecursive == false)
+            return isSubsetSum(arr,sum/2);
+        else {
+            return isSubsetSum(arr,sum/2, arr.size());
+        }
     }
 }
 
+
+
 int main() {
     vector<int>arr {1,5,11,5};
-    cout<<isEqualSumPartition(arr)<<endl;
+    cout<<isEqualSumPartition(arr, false)<<endl;
+
+    cout<<isEqualSumPartition(arr, true)<<endl;
 }
