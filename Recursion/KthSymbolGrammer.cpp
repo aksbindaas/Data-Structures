d4d40e82f355e63f43e83ad3d6a106faa596d6b6
Ankit Srivastava <ankit.kumar@mediakind.com>
2022-01-27 17:09:58 +0530

    We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
    For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
    Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table of n rows.
    K=>    1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16
    N = 1  0
    N = 2  0  1
    N = 3  0  1  1  0
    N = 4  0  1  1  0  1  0  0  1
    N = 5  0  1  1  0  1  0  0  1  1  0  0  1  0  1  1  0

---
+++ Recursion/KthSymbolGrammer.cpp
@@ -0,0 +1,36 @@
+/*
+We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
+
+For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
+Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table of n rows.
+K=>    1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16
+N = 1  0
+N = 2  0  1
+N = 3  0  1  1  0
+N = 4  0  1  1  0  1  0  0  1
+N = 5  0  1  1  0  1  0  0  1  1  0  0  1  0  1  1  0
+*/
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int solve(int n , int k ) {
+    if(n == 1 && k == 1) {
+        return 0;
+    }
+    int mid = pow(2,n-1)/2;
+    if(k<= mid) {
+        return solve(n-1, k);
+    } else {
+        return !solve(n-1, k-mid);
+    }
+}
+
+int main() {
+    for (int i = 1; i<=16; i++) {
+     cout<<solve(5,i)<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
